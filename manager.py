0fae21bba4c988e06bcfc4768de4316073970707
Elibon123 <eli@vhelectronics.com>
2026-07-04 08:56:47 +0200

    Refactor task manager functions for better usability
    Refactor task manager functions to accept a task list as an argument and improve user interaction.

--- manager.py
+++ manager.py
@@ -1,38 +1,37 @@
 #Task manager
-#from src.knihovna import zobrazit_knihy
-ukoly = []
+#Funkce hlavni_menu zobrazuje hlavni menu spravce ukolu a poskytuje uzivateli moznosti 
+# pridat, zobrazit nebo odstranit ukoly, nebo ukoncit program.
 def hlavni_menu():
-    print("Spravce ukolu - Hlavni menu")
-    print("1. Pridat novy ukol")
-    print("2. Zobrazit vsechny ukoly")
-    print("3. Odstranit ukol")
-    print("4. Konec programu")
-
-    moznost = input("Vyberte moznost (1-4): ").strip()  
+    ukoly = []
     while True:
+        print("Spravce ukolu - Hlavni menu")
+        print("1. Pridat novy ukol")
+        print("2. Zobrazit vsechny ukoly")
+        print("3. Odstranit ukol")
+        print("4. Konec programu")
+
+        moznost = input("Vyberte moznost (1-4): ").strip()  
         if moznost == "1":
-            pridat_ukol()
+            pridat_ukol(ukoly)
         elif moznost == "2":
-            zobrazit_ukoly()
+            zobrazit_ukoly(ukoly)
         elif moznost == "3":
-            odstranit_ukol()
+            odstranit_ukol(ukoly)
         elif moznost == "4":
             print("Konec programu.")
+            break
         else:
-            moznost = input(f"Neplatnou volbu. Vyberte moznost (1-4): ")
+            print(f"Neplatnou volbu. Vyberte moznost (1-4): ")
 
-#if __name__ == "__main__":
-##    conn = pripojeni_k_databazi()
-#    if conn:
-#        vytvoreni_tabulky(conn)
-#        vytvor_data(conn)
 
-#conn.close()
 
-def pridat_ukol():
+#Funkce pridava novy ukol do seznamu ukolu, 
+# pokud je nazev nebo popis prazdny, vypise hlasku "Zadan je prazdny nazev" 
+# nebo "Zadan je prazdny popis" 
+def pridat_ukol(ukoly):
     while True:
-        nazev_ukolu = input("Zadejte nazev ukolu: ")
-        popis_ukolu = input("Zadejte popis ukolu: ")
+        nazev_ukolu = input("Zadejte nazev ukolu: ").strip()
+        popis_ukolu = input("Zadejte popis ukolu: ").strip()
         kontrola_nazvu = len(nazev_ukolu)
         kontrola_popisu = len(popis_ukolu)
         if kontrola_nazvu == 0:
@@ -40,36 +39,38 @@ def pridat_ukol():
         elif kontrola_popisu == 0:
             print("Zadan je prazdny popis")
         else:
-            number = len(ukoly) + 1
-            ukoly.append({"number": number, "nazev": nazev_ukolu, "popis": popis_ukolu})
+            cislo = len(ukoly) + 1
+            ukoly.append({"cislo": cislo, "nazev": nazev_ukolu, "popis": popis_ukolu})
             print(f"Ukol '{nazev_ukolu}' byl pridan!")
-            hlavni_menu()
             break
 
-def zobrazit_ukoly():
-        for ukol in ukoly:
-            print("Seznam ukolu:")
-            print(f"{ukol['number']}. {ukol['nazev']} - {ukol['popis']}")
-            hlavni_menu()
-            break
+#Funkce zobrazuje seznam ukolu, pokud nejsou zadne ukoly, vypise hlasku "Zadne ukoly"
+def zobrazit_ukoly(ukoly):
+    if not ukoly:
+        print("Zadne ukoly")
+    else:
+        print("Seznam ukolu:")
+        for cislo, ukol in enumerate(ukoly, 1):
+            print(f"{cislo}. {ukol['nazev']} - {ukol['popis']}")
 
 
-def odstranit_ukol():
+
+#Funkce odstranuje ukol podle zadaneho cisla
+def odstranit_ukol(ukoly):
     if not ukoly:
         print("Zadne ukoly nejsou k dispozici k odstraneni.")
     else:
         print("Seznam ukolu:")
-        for ukol in ukoly:
-            print(f"{ukol['number']}. {ukol['nazev']} - {ukol['popis']}")
+        for cislo, ukol in enumerate(ukoly, 1):
+            print(f"{cislo}. {ukol['nazev']} - {ukol['popis']}")
         while True:
-                volba = int(input("Zadejte cislo ukolu, ktery chcete odstranit: "))
-                if 1 <= volba <= len(ukoly):
-                    odstraneny_ukol = ukoly.pop(volba - 1)
-                    print(f"Ukol '{odstraneny_ukol['nazev']}' byl odstraněn!")
-                    hlavni_menu()
-                    break
-                else:
-                    print(f"Neplatna volba. Zadejte cislo mezi 1 a {len(ukoly)}.")
+            volba = int(input("Zadejte cislo ukolu, ktery chcete odstranit: "))
+            if 1 <= volba <= len(ukoly):
+                odstraneny_ukol = ukoly.pop(volba - 1)
+                print(f"Ukol '{odstraneny_ukol['nazev']}' byl odstraněn!")
+                break
+            else:
+                print(f"Neplatna volba. Zadejte cislo mezi 1 a {len(ukoly)}.")
 
 hlavni_menu()
 
